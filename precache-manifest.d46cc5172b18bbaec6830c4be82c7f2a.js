790569bf2b6e34d95f6b71163c8caf8ff4b20cb8
Julio <jc.mussi99@gmail.com>
2020-03-11 22:20:23 -0300

    Updates

---
+++ precache-manifest.d46cc5172b18bbaec6830c4be82c7f2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f12e276110e5c3c8b233c760dba874c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33fd59c7e6991d839dcc",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0cc53fee184ec953778d",
+    "url": "/static/js/2.27b98db5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.27b98db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33fd59c7e6991d839dcc",
+    "url": "/static/js/main.e7c00ba9.chunk.js"
+  },
+  {
+    "revision": "ed992bab683700dfcc84",
+    "url": "/static/js/runtime-main.5fff0ba7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
